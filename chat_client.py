129942b39e35ce7f0219e8ee980488ce0d4ddc74
Tanguy Defer <tanguy.defer-ext@pole-emploi.fr>
2022-06-11 23:38:14 +0200

    Add part & cmd_msg function

--- chat_client.py
+++ chat_client.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
+import logging
 import re
 import sys
 import threading
-import logging
 
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 
 # Configuration du logger
 log = logging.getLogger("chat_client")
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler(sys.stdout))
+
 should_quit = False
+SUB_CHANNELS = []
 
 
 def read_messages(consumer):
@@ -27,33 +29,85 @@ def read_messages(consumer):
 
 
 
-def cmd_msg(producer, channel, line):
-    # TODO À compléter
-    pass
+def cmd_msg(producer, curchan, args, nick_name):
+    if curchan:
+        try:
+            log.info("Sending message to %s ...", curchan)
+            formated_curchan = "chat_channel_" + curchan[1:]
+            message = nick_name + ": " + args
+            producer.send(formated_curchan, str.encode(message))
+            log.info("message sent by %s on channel %s", nick_name, curchan)
+        except Exception as err:
+            log.error("Impossible to send message ... %s", err)
+    else:
+        log.warning("No active channel")
 
 
-def cmd_join(consumer, producer, args):
+def cmd_join(nick_name,consumer, producer, args):
     try:
-        re.match(r'^#[a-zA-Z0-9_-]+$', args)
         consumer.subscribe("chat_channel_" + args[1:])
-        log.info("Subscribed to : %s", args[1:])
+        if args not in SUB_CHANNELS:
+            SUB_CHANNELS.append(args)
+        log.info("%s has joined chat channel %s",nick_name, args[1:])
+        log.info("List of %s's channels : %s",nick_name, SUB_CHANNELS)
         return True
     except Exception as err:
-        log.error("ERROR: %s", err)
+        log.error("Subscribe to : %s failed", err)
         return False
 
 
-def cmd_part(consumer, producer, line):
-    # TODO À compléter
-    pass
+def cmd_part(nick_name, consumer, producer, args):
+    """function to quit a channel and join one of user's channels
 
+    Args:
+        consumer (_type_): kaka consumer
+        producer (_type_): kafka producer
+        args (_type_): channel to quit
+    Returns:
+        _type_: subscribe channel or False
+    """
+    if args in SUB_CHANNELS:
+        if len(SUB_CHANNELS) < 1:
+            log.warning("No channel subscribe")
+            return None
+        else:
+            print("ARGS IN PART :" , args)
+            consumer.unsubscribe()
+            log.info("%s has left chat channel : %s",nick_name, args[1:])
+            SUB_CHANNELS.remove(args)
+            # TODO attention création d'un  new chan pas bon
+            cmd_join(nick_name,consumer, producer, SUB_CHANNELS[-1])
+            return True
+    else:
+        log.error("%s is not in your channels", args)
+        return False
 
 def cmd_quit(producer, line):
     # TODO À compléter
     pass
 
+def channels_in_topic():
+    """function to transform #general to chat_channel_general"""
+    formated_channels = []
+    for channel in SUB_CHANNELS:
+        formated_channels.append("chat_channel_" + channel[1:])
+    return formated_channels
 
-def main_loop(nick, consumer, producer):
+
+def check_channel_format(args):
+    """check if args format is correct
+    Args:
+        args (_type_): #general
+
+    Returns:
+        True or False
+    """
+    if re.match(r'^#[a-zA-Z0-9_-]+$', args):
+        return True
+    log.error("Incorrect channel format for '%s', channel format must be like '#general'", args)
+    return False
+
+def main_loop(nick_name, consumer, producer):
     curchan = None
 
     while True:
@@ -63,7 +117,7 @@ def main_loop(nick, consumer, producer):
             else:
                 line = input("[#%s]> " % curchan)
         except EOFError:
-            print("/quit")
+            log.error("/quit")
             line = "/quit"
 
         if line.startswith("/"):
@@ -75,30 +129,31 @@ def main_loop(nick, consumer, producer):
             args = line
 
         if cmd == "msg":
-            cmd_msg(producer, curchan, args)
+            cmd_msg(producer, curchan, args, nick_name)
         elif cmd == "join":
-            if cmd_join(consumer, producer, args):
+            if check_channel_format(args) and cmd_join(nick_name, consumer, producer, args):
                 curchan = args
         elif cmd == "part":
-            cmd_part(consumer, producer, args)
+            return_value = cmd_part(nick_name,consumer, producer, args)
+            if return_value or None:
+                curchan = SUB_CHANNELS[-1]
         elif cmd == "quit":
             cmd_quit(producer, args)
             break
-        # TODO: rajouter des commandes ici
 
 
 def main():
     if len(sys.argv) != 2:
-        print("usage: %s nick" % sys.argv[0])
+        print("usage: %s nick_name" % sys.argv[0])
         return 1
 
-    nick = sys.argv[1]
+    nick_name = sys.argv[1]
     consumer = KafkaConsumer()
     producer = KafkaProducer()
     th = threading.Thread(target=read_messages, args=(consumer,))
     th.start()
     try:
-        main_loop(nick, consumer, producer)
+        main_loop(nick_name, consumer, producer)
     finally:
         global should_quit
         should_quit = True
